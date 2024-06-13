b644a9d731425a5f52572403b2ce0331bbb4960c
Omeir Ahmed <omair.ahmed@vyro.ai>
2024-06-13 23:18:46 +0500

    Initial Commit

---
+++ TranslationApp/TranslationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationAppApp.swift
+//  TranslationApp
+//
+//  Created by M on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TranslationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
